b217fdacb38c4daa9ed3e96cdc88bf711517cfea
ysfzyak <ysfzyak@gmail.com>
2022-12-03 15:07:44 +0300

    Login ve Register Ekranları Oluşturuldu.

---
+++ app/src/main/java/com/example/deneme/SignInActivity.kt
@@ -0,0 +1,15 @@
+package com.example.deneme
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.deneme.R.layout.activity_signin
+
+class SignInActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(activity_signin)
+    }
+
+
+}
---
+++ app/src/main/java/com/example/deneme/SignUp.kt
@@ -0,0 +1,4 @@
+package com.example.deneme
+
+class SignUp {
+}
