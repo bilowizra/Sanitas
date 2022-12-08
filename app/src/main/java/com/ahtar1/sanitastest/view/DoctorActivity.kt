ce8e62d89b38da660ccedf2b5b2fc54905312beb
ridvanh <ridvanhocaoglu1@gmail.com>
2022-12-08 15:14:53 +0300

    added menubar for doctor

--- app/src/main/java/com/ahtar1/sanitastest/view/DoctorActivity.kt
+++ app/src/main/java/com/ahtar1/sanitastest/view/DoctorActivity.kt
@@ -2,11 +2,39 @@ package com.ahtar1.sanitastest.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.Profile
+import androidx.fragment.app.Fragment
 import com.ahtar1.sanitastest.R
+import com.ahtar1.sanitastest.databinding.ActivityDoctorBinding
+import com.ahtar1.sanitastest.doctor_profile
+import com.ahtar1.sanitastest.doctor_schedule
+import kotlinx.android.synthetic.main.activity_doctor.*
 
 class DoctorActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityDoctorBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_doctor)
+        binding = ActivityDoctorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(doctor_profile())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.profile -> replaceFragment(doctor_profile())
+                R.id.schedule -> replaceFragment(doctor_schedule())
+                else -> { }
+            }
+            true
+        }
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
