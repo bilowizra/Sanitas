3b788f53fd5a924abd8f7f9fcc5de55fb8955487
ysfzyak <ysfzyak@gmail.com>
2022-12-09 00:05:49 +0300

    Doctor Profile Screen is designed.

--- app/src/main/java/com/ahtar1/sanitastest/view/doctor_profile.kt
+++ app/src/main/java/com/ahtar1/sanitastest/view/doctor_profile.kt
@@ -6,13 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.ahtar1.sanitastest.R
+import com.google.firebase.auth.FirebaseAuth
 
 
 class doctor_profile : Fragment() {
 
+    private lateinit var auth: FirebaseAuth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
     }
 
